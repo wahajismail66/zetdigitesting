38d81910021b1c6fecd574b6ca43a13095d70b5e
Wahaj Ismail <163578922+wahajismail66@users.noreply.github.com>
2025-04-16 18:00:53 +0500

    Create MidSection.jsx

---
+++ MidSection.jsx
@@ -0,0 +1,41 @@
+export const MidSection = () => {
+    return(
+        <>
+        <div className="mid-container">
+            <div className="mid-heading">
+                <h1>What Are the Benefits of Using Online Mixing and Mastering Services?</h1>
+            </div>
+            <div className="photo-para">
+                <div className="photo"><img src="../../public/img/mid-photo.webp"></img></div>
+                <div className="para"><p>The biggest benefit of using online mixing and mastering services is that you can save a lot of money and use the rest for things like marketing, video shoots, and promotion, all of which are expensive to begin.</p>
+                <p>Mixing is essentially the process before mastering, and it entails integrating and modifying separate recordings to make a professional stereo mix. Next, the stereo file is mastered to ensure the music has been cleaned and framed into a powerful, radio-ready release smash hit.</p>
+                <p>Musicians nowadays have easy access to recording software and gear. What is great about this rise in self-recording is that it allows independent musicians in the seclusion of their own homes to make multi-track recordings at their leisure.</p>
+                <p>You may save money by hiring a mixing and mastering firm and putting the spare time and money to good use. Additionally, we offer Customer Service 24 hours a day, 7 days a week, right here on our website. Using our online chat technology, we can communicate swiftly and easily as we genuinely care about the end product’s quality.</p>
+                <p>Before sending us your project, please examine our upload page and send the split tracks so that we can work on each one separately. Remember, we embrace all genres, so prepare your files and let us take your music to the next level!</p>
+                </div>
+            </div>
+        </div>
+        <div className="mid-container-second">
+                <div className="mid-heading"><h1>What Are the Risks of Not Using Professional Online Mixing and Mastering Services?</h1></div>
+                <div className="photo-para">
+                    <div className="para-2"><p>Professional mixing and mastering services are an essential component of any musician’s career, whether they are just starting out in a garage or selling multimillion-dollar albums. Without expert mixing services and complete control over the listener’s experience, you risk appearing unprofessional and jeopardizing the integrity of your music. However, you have options if you want to know how to acquire the finest potential outcomes for your music. It is entirely up to you whether you want to take the chance that your music will not sound as it should or hire Online music mastering service professional. Seriously, while determining where you want to save money, keep your future in mind. We would strongly recommend hiring us to do your job if you desire high-quality sound. You can hear why we are the best in the world by listening to our before and after samples below.</p></div>
+                    <div className="photo-2-mid"><img src="../../public/img/second-mid-photo.webp"></img></div>
+                </div>
+            </div>
+
+        <div className="mid-container-third">
+            <div className="mid-heading"><h1>Mixing and Mastering Services</h1></div>
+            <div className="photo-para">
+                <div className="photo-3-mid"><img src="../../public/img/third-mid-photo.webp"></img></div>
+                <div className="para-and-btn">
+                    <div className="para-3"><p>The music industry is one of the most competitive in the world. Mixing and mastering services are essential for any artist or recording team that is looking to achieve success in their career. From vocal to instrumental, mixing and mastering services are needed to ensure that the final product sounds professional and polished. With accurate mixing and mastering, artists can plan their releases and submit their songs to record labels much easier.</p>
+                    <p>What is Mixing and Mastering Service? Mixing is the term commonly used to refer to the process of taking an audio track and cleaning it solidly using advance techniques.</p>
+                    <p>Mastering is the final polish in audio production, which involves enhancing the overall sound, creating consistency, making the song sound professional and radio ready.</p>
+                    </div>
+                    <div className="lm-button"><button>Learn More</button></div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}

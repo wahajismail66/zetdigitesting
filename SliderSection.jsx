825f1887927aae212a4eafa9ff6b8b3778683c64
Wahaj Ismail <163578922+wahajismail66@users.noreply.github.com>
2025-04-16 18:01:34 +0500

    Create SliderSection.jsx

---
+++ SliderSection.jsx
@@ -0,0 +1,10 @@
+import { SlickSlider } from "./SlickSlider"
+
+export const SliderSection = () =>{
+    return <div className="slider-container">
+    <div className="slider-heading"><h3 className="slide-head-1">Just A Few Of Many</h3>
+    <h1 className="slide-head-2">Name Credits</h1>
+    </div>
+    <SlickSlider/>
+    </div>
+}

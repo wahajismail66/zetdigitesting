a5b7447bfbc7f783cc769fd5f08f76e9500bf3ff
Wahaj Ismail <163578922+wahajismail66@users.noreply.github.com>
2025-04-16 18:00:21 +0500

    Create HeroSection.jsx

---
+++ HeroSection.jsx
@@ -0,0 +1,13 @@
+export const HeroSection = () => {
+    return (
+        <div className="container">
+            <div className="hero-heading">
+                <h1>Professional Online</h1>
+                <h1>Mixing and Mastering Services At
+                    A Very Affordable Price!</h1>
+            </div>
+            <div className="sample-button"><button>More Samples</button></div>
+            <div className="hero-image"><img src="../../public/img/hero-section-image.webp"></img></div>
+        </div>
+    )
+}
